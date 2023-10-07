3edd4e193cb479e7e0e7d4ac032f375987460ca8
singhyashe15 <singhyashraj574@gmail.com>
2023-10-08 00:49:18 +0530

    Add new  file

---
+++ script.js
@@ -0,0 +1,57 @@
+const currDate = document.querySelector('.Date');
+const currIcon = document.querySelector('.Weather-icon');
+
+// const tempIcon = "{%tempIcon%}";
+// console.log(tempIcon);
+
+// if(tempIcon == "Sunny")
+// {
+//   currIcon.innerHTML = "<i class='fa-thin fa-sun fa-fade fa-2xl'></i>";
+// }
+// else if(tempIcon == "Clouds")
+// {
+//   currIcon.innerHTML = <i class='fa-regular fa-cloud'></i>;
+// }
+// else if(tempIcon == "Rainy")
+// {
+//   currIcon.innerHTML = "<i class='fa-solid fa-cloud-showers-heavy'></i>";
+// }
+// else
+// {
+//   currIcon.innerHTML = <i class="fa-light fa-sun"></i>;
+// }
+const currDay = ()=>{
+  var DayArrray = [
+    "Sun","Mon","Tues","Wed","Thurs","Fri","Sat"
+  ];
+  let currday = new Date().getDay();
+
+  return DayArrray[currday];
+};
+
+
+const currTime = ()=>{
+  var Month = [
+    "Jan","Feb","Mar","Aprl","May","Jun","July","Aug","Sept","Oct","Nov","Dec"];
+
+    const currmonth = new Date().getMonth();
+    console.log(currmonth);
+    const currDate = new Date().getDate();
+    const currHours = new Date().getHours();
+    const currMin = new Date().getMinutes();
+    const currSec = new Date().getSeconds();
+
+    if(currMin < 10)
+    {
+      currMin = "0" + currMin;
+    }
+    if(currSec < 10)
+    {
+      currSec = "0" + currSec;
+    }
+    // console.log(Month[currmonth]);
+
+    return `${Month[currmonth]}${currDate} || ${currHours}:${currMin}:${currSec} ||`;
+}
+
+currDate.innerHTML = currTime() + currDay();
